804730db48b3e5af79c0d08c1836abbee7370288
Heidi Hlavinka <hchlav@gmail.com>
2018-06-07 14:21:54 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,8 +7,9 @@ def create_project_hash
   binding.pry
   projects = {}
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
-end
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
   projects
 end  
 
